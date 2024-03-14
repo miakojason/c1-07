f69a8b4be2f174a0fccd5fee976ee56c22d24d91
miakojason <miakojason@gmail.com>
2024-03-14 13:32:17 +0800

    back ad ok + api edit ok

---
+++ back/ad.php
@@ -0,0 +1,39 @@
+<div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
+    <p class="t cent botli">動態文字廣告管理</p>
+    <form method="post" action="./api/edit.php" enctype="multipart/form-data">
+        <table width="100%">
+            <tbody>
+                <tr class="yel">
+                    <td width="45%">動態文字廣告</td>
+                    <td width="7%">顯示</td>
+                    <td width="7%">刪除</td>
+                </tr>
+                <?php
+                $rows = $DB->all();
+                foreach ($rows as $row) {
+                ?>
+                <tr>
+                    <td><input style="width: 90%;" type="text" name="text[]" value="<?=$row['text'];?>"></td>
+                    <td><input type="checkbox" name="sh[]" value="<?=$row['id'];?>"<?=($row['sh']==1)?'checked':'';?>></td>
+                    <td>
+                        <input type="hidden" name="id[]" value="<?=$row['id'];?>">
+                        <input type="checkbox" name="del[]" value="<?=$row['id'];?>">
+                    </td>
+                </tr>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
+        <table style="margin-top:40px; width:70%;">
+            <tbody>
+                <tr>
+                    <input type="hidden" name="table" value="<?=$do;?>">
+                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./model/<?= $do; ?>.php')" value="新增動態文字廣告"></td>
+                    <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
+                </tr>
+            </tbody>
+        </table>
+
+    </form>
+</div>
--- back/title.php
+++ back/title.php
@@ -11,21 +11,21 @@
                     <td></td>
                 </tr>
                 <?php
-                $rows = $Title->all();
+                $rows = $DB->all();
                 foreach ($rows as $row) {
                 ?>
-                <tr>
-                    <td><img src="./img/<?=$row['img'];?>" style="width:300px;height:30px;"></td>
-                    <td><input type="text" name="text[]" value="<?=$row['text'];?>"></td>
-                    <td><input type="radio" name="sh" value="<?=$row['id'];?>"<?=($row['sh']==1)?'checked':'';?>></td>
-                    <td>
-                        <input type="hidden" name="id[]" value="<?=$row['id'];?>">
-                        <input type="checkbox" name="del[]" value="<?=$row['id'];?>">
-                    </td>
-                    <td>
-                        <input type="button" value="更新圖片" onclick="op('#cover','#cvr','./model/upload.php?table=<?=$do;?>&id=<?=$row['id'];?>')" value="更新圖片">
-                    </td>
-                </tr>
+                    <tr>
+                        <td><img src="./img/<?= $row['img']; ?>" style="width:300px;height:30px;"></td>
+                        <td><input type="text" name="text[]" value="<?= $row['text']; ?>"></td>
+                        <td><input type="radio" name="sh" value="<?= $row['id']; ?>" <?= ($row['sh'] == 1) ? 'checked' : ''; ?>></td>
+                        <td>
+                            <input type="hidden" name="id[]" value="<?= $row['id']; ?>">
+                            <input type="checkbox" name="del[]" value="<?= $row['id']; ?>">
+                        </td>
+                        <td>
+                            <input type="button" value="更新圖片" onclick="op('#cover','#cvr','./model/upload.php?table=<?= $do; ?>&id=<?= $row['id']; ?>')" value="更新圖片">
+                        </td>
+                    </tr>
                 <?php
                 }
                 ?>
@@ -34,6 +34,7 @@
         <table style="margin-top:40px; width:70%;">
             <tbody>
                 <tr>
+                    <input type="hidden" name="table" value="<?= $do; ?>">
                     <td width="200px"><input type="button" onclick="op('#cover','#cvr','./model/<?= $do; ?>.php')" value="新增網站標題圖片"></td>
                     <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
                 </tr>
