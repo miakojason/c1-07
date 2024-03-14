bc3dbf0f404a25d0319136c4abe4358f9564d20a
miakojason <miakojason@gmail.com>
2024-03-14 11:51:08 +0800

    back title ok

--- back/title.php
+++ back/title.php
@@ -1,6 +1,6 @@
 <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
     <p class="t cent botli">網站標題管理</p>
-    <form method="post"  action="./api/edit.php" enctype="multipart/form-data">
+    <form method="post" action="./api/edit.php" enctype="multipart/form-data">
         <table width="100%">
             <tbody>
                 <tr class="yel">
@@ -10,12 +10,31 @@
                     <td width="7%">刪除</td>
                     <td></td>
                 </tr>
+                <?php
+                $rows = $Title->all();
+                foreach ($rows as $row) {
+                ?>
+                <tr>
+                    <td><img src="./img/<?=$row['img'];?>" style="width:300px;height:30px;"></td>
+                    <td><input type="text" name="text[]" value="<?=$row['text'];?>"></td>
+                    <td><input type="radio" name="sh" value="<?=$row['id'];?>"<?=($row['sh']==1)?'checked':'';?>></td>
+                    <td>
+                        <input type="hidden" name="id[]" value="<?=$row['id'];?>">
+                        <input type="checkbox" name="del[]" value="<?=$row['id'];?>">
+                    </td>
+                    <td>
+                        <input type="button" value="更新圖片" onclick="op('#cover','#cvr','./model/upload.php?table=<?=$do;?>&id=<?=$row['id'];?>')" value="更新圖片">
+                    </td>
+                </tr>
+                <?php
+                }
+                ?>
             </tbody>
         </table>
         <table style="margin-top:40px; width:70%;">
             <tbody>
                 <tr>
-                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','view.php?do=title')" value="新增網站標題圖片"></td>
+                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./model/<?= $do; ?>.php')" value="新增網站標題圖片"></td>
                     <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
                 </tr>
             </tbody>
