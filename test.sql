893042a5d1c1d7bd902358c198683c05ca6cb738
miakojason <miakojason@gmail.com>
2024-03-14 10:57:58 +0800

    初步切版整理ok

---
+++ test.sql
@@ -0,0 +1,85 @@
+INSERT INTO `ad` (`id`, `text`, `sh`) VALUES (NULL, '轉知臺北教育大學與臺灣師大合辦第11屆麋研齋全國硬筆書法比賽活動', '1');
+INSERT INTO `ad` (`id`, `text`, `sh`) VALUES (NULL, '轉知:法務部辦理「第五屆法規知識王網路闖關競賽辦法', '1');
+INSERT INTO `ad` (`id`, `text`, `sh`) VALUES (NULL, '轉知2012年全國青年水墨創作大賽活動', '1');
+INSERT INTO `ad` (`id`, `text`, `sh`) VALUES (NULL, '欣榮圖書館101年悅讀達人徵文比賽，歡迎全校師生踴躍投稿參加', '1');
+INSERT INTO `ad` (`id`, `text`, `sh`) VALUES (NULL, '轉知:教育是人類升沉的樞紐-2013教師生命成長營', '1');
+轉知臺北教育大學與臺灣師大合辦第11屆麋研齋全國硬筆書法比賽活動
+轉知:法務部辦理「第五屆法規知識王網路闖關競賽辦法
+轉知2012年全國青年水墨創作大賽活動
+欣榮圖書館101年悅讀達人徵文比賽，歡迎全校師生踴躍投稿參加
+轉知:教育是人類升沉的樞紐-2013教師生命成長營
+-- 
+INSERT INTO `news` (`id`, `text`, `sh`) VALUES (NULL, '教師研習「世界公民生命園丁國內研習會」
+1.主辦單位：世界展望會
+2.研習日期：101年11月14日（三）～15日（四）
+3.詳情請參考：
+http://gc.worldvision.org.tw/seed.html。
+請線上報名。', '1');
+INSERT INTO `news` (`id`, `text`, `sh`) VALUES (NULL, '公告綜合高中一年級英數補救教學時間
+上課日期:10/27.11/3.11/10.11/24共計四次
+上課時間:早上8:00~11:50半天
+費用:全程免費
+參加同學:綜合科一年級第一次段考成績需加強者
+已將名單送交各班及導師
+參加同學請帶紙筆.課本.第一次段考考卷
+並將家長通知單給家長
+若有任何疑問
+請洽綜合高中學程主任', '1');
+INSERT INTO `news` (`id`, `text`, `sh`) VALUES (NULL, '102年全國大專校院運動會
+「主題標語及吉祥物命名」
+網路票選活動
+一、活動期間：自10月25日起至11月4日止。
+二、相關訊息請上宜蘭大學首頁連結「102全大運在宜大」
+活動網址：http://102niag.niu.edu.tw/', '1');
+INSERT INTO `news` (`id`, `text`, `sh`) VALUES (NULL, '台灣亞洲藝術文化教育交流學會第一屆年會國際研討會
+活動日期：101年3月3～4日(六、日)
+活動主題：創造力、文化、全人教育
+有意參加者請至http://www.caaetaiwan.org下載報名表', '1');
+INSERT INTO `news` (`id`, `text`, `sh`) VALUES (NULL, '
+11月23日(星期五)將於彰化縣田尾鄉菁芳園休閒農場
+舉辦「高中職生涯輔導知能研習」
+中區學校每校至多2名
+以普通科、專業類科教師優先報名參加
+生涯規劃教師次之，參加人員公差假
+並核實派代課
+當天還有專車接送(8:35前在員林火車站集合)
+如此好康的機會，怎能錯過？！
+熱烈邀請師長們向輔導室(分機234)報名
+名額有限，動作要快！！
+報名截止日期：本周四 10月25日17:00前！', '1');
+INSERT INTO `news` (`id`, `text`, `sh`) VALUES (NULL, '台視百萬大明星節目辦理海選活動
+時間:101年10月27日下午13時
+地點:彰化', '1');
+INSERT INTO `news` (`id`, `text`, `sh`) VALUES (NULL, '國立故宮博物院辦理
+「商王武丁與后婦好 殷商盛世文化藝術特展」暨
+「赫赫宗周 西周文化特展」', '1');
+INSERT INTO `news` (`id`, `text`, `sh`) VALUES (NULL, '財團法人漢光教育基金會
+辦理2012「舊愛新歡-古典詩詞譜曲創作暨歌唱表演競賽」
+參賽獎金豐厚!!', '1');
+-- 
+INSERT INTO `titles` (`id`, `img`, `text`, `sh`) VALUES (NULL, '01B01.jpg', '卓越科技大學校園資訊系統', '1');
+INSERT INTO `titles` (`id`, `img`, `text`, `sh`) VALUES (NULL, '01B02.jpg', '卓越科技大學校園資訊系統', '0');
+INSERT INTO `titles` (`id`, `img`, `text`, `sh`) VALUES (NULL, '01B03.jpg', '卓越科技大學校園資訊系統', '0');
+INSERT INTO `titles` (`id`, `img`, `text`, `sh`) VALUES (NULL, '01B04.jpg', '卓越科技大學校園資訊系統', '0');
+-- 
+INSERT INTO `image` (`id`, `img`, `sh`) VALUES (NULL, '01D01.jpg', '1');
+INSERT INTO `image` (`id`, `img`, `sh`) VALUES (NULL, '01D02.jpg', '1');
+INSERT INTO `image` (`id`, `img`, `sh`) VALUES (NULL, '01D03.jpg', '1');
+INSERT INTO `image` (`id`, `img`, `sh`) VALUES (NULL, '01D04.jpg', '1');
+INSERT INTO `image` (`id`, `img`, `sh`) VALUES (NULL, '01D05.jpg', '1');
+INSERT INTO `image` (`id`, `img`, `sh`) VALUES (NULL, '01D06.jpg', '1');
+INSERT INTO `image` (`id`, `img`, `sh`) VALUES (NULL, '01D07.jpg', '1');
+INSERT INTO `image` (`id`, `img`, `sh`) VALUES (NULL, '01D08.jpg', '1');
+INSERT INTO `image` (`id`, `img`, `sh`) VALUES (NULL, '01D09.jpg', '1');
+INSERT INTO `image` (`id`, `img`, `sh`) VALUES (NULL, '01D10.jpg', '1');
+-- 
+INSERT INTO `mvim` (`id`, `img`, `sh`) VALUES (NULL, '01C01.gif', '1');
+INSERT INTO `mvim` (`id`, `img`, `sh`) VALUES (NULL, '01C02.gif', '1');
+INSERT INTO `mvim` (`id`, `img`, `sh`) VALUES (NULL, '01C03.gif', '1');
+INSERT INTO `mvim` (`id`, `img`, `sh`) VALUES (NULL, '01C04.gif', '1');
+INSERT INTO `mvim` (`id`, `img`, `sh`) VALUES (NULL, '01C05.gif', '1');
+INSERT INTO `mvim` (`id`, `img`, `sh`) VALUES (NULL, '01C06.gif', '1');
+-- 
+INSERT INTO `menu` (`id`, `text`, `href`, `menu_id`, `sh`) VALUES (NULL, '管理登入', '?do=login', '0', '1');
+INSERT INTO `menu` (`id`, `text`, `href`, `menu_id`, `sh`) VALUES (NULL, '網站首頁', './index.php', '0', '1');
+INSERT INTO `menu` (`id`, `text`, `href`, `menu_id`, `sh`) VALUES (NULL, '更多內容', '?do=news', '2', '1');
